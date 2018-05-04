a027529ed1e0decfe0006d5413a93a824f1482b6
pangwong <pengwang0615@gmail.com>
2018-05-04 15:04:18 +0800

    minor fix

--- models/resnet.py
+++ models/resnet.py
@@ -167,6 +167,7 @@ class ResNetTemplet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
