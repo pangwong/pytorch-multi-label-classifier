38e1792c84408a0e840e5a27742fdaa6a1efde40
pangwong <pengwang0615@gmail.com>
2018-05-07 10:08:26 +0800

    update demo

--- demo.sh
+++ demo.sh
@@ -11,7 +11,8 @@
 #
 # 4. uncomment to train
 #
-# python multi_label_classifier.py --dir "./test/celeba/" --mode "Train" --name "test" --batch_size 64 --gpu_ids 0 --input_channel 3 --load_size 144 --input_size 128 --mean [0,0,0] --std [1,1,1] --ratio "[0.94, 0.03, 0.03]" --shuffle --load_thread 8 --sum_epoch 20 --lr_decay_in_epoch 4 --display_port 8900 --validate_ratio 0.1 --top_k "(1,)" --score_thres 0.1 --display_train_freq 20 --display_validate_freq 20 --save_epoch_freq 1  --display_image_ratio 0.2
+# python multi_label_classifier.py --dir "./test/celeba/" --mode "Train" --model VGG16 --name "test" --batch_size 64 --gpu_ids 0 --input_channel 3 --load_size 144 --input_size 128 --mean [0,0,0] --std [1,1,1] --ratio "[0.94, 0.03, 0.03]" --shuffle --load_thread 8 --sum_epoch 20 --lr_decay_in_epoch 4 --display_port 8900 --validate_ratio 0.1 --top_k "(1,)" --score_thres 0.1 --display_train_freq 20 --display_validate_freq 20 --save_epoch_freq 1  --display_image_ratio 0.2
+#
 # 5. open localhost:8900 on your browser and you will see loss and accuracy curves and training images samples later on.
 #
 #--------------test--------------
